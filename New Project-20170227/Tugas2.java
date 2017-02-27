3d1feb5e26c7f4233b5539f7de814518b5767e46
ChoirunNazilin <cnazilin@gmail.com>
2017-02-27 11:15:03 +0700

    Created by Coding Ground Client

---
+++ New Project-20170227/Tugas2.java
@@ -0,0 +1,9 @@
+public class Tugas2{
+
+     public static void main(String []args){
+        String[] nama = {"Choirun", "Nazilin"};
+        for (int i = 0; i < nama.length; i++) {
+         System.out.println(nama[i] + " ");
+      }
+     }
+}
