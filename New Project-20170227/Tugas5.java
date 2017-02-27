bca023009b4cc3a43367f334d765ba455f2ed6a2
ChoirunNazilin <cnazilin@gmail.com>
2017-02-27 12:15:25 +0700

    Created by Coding Ground Client

---
+++ New Project-20170227/Tugas5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Tugas5 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Masukkan angka =");
+        int a = scanner.nextInt();
+        int hasil = hitungmundur(a);
+        for ( int i = hasil; i>= 1; i-- ){
+            System.out.println( i );
+        }
+    }
+    
+    public static int hitungmundur(int a1){
+    int angka = a1;
+    
+    return angka;
+    
+        
+    }
+}
